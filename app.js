54c3f67fb975cf91fc634da1a7161f9a68e1e5d5
Amine <aminkarmous2000@gmail.com>
2022-03-01 16:11:14 +0100

    make it better

---
+++ app.js
@@ -0,0 +1,36 @@
+const express=require('express');
+const app=express();
+const mongoose=require('mongoose');
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const cors=require('cors')
+const expressValidator=require('express-validator');
+//import routes
+const authRoutes=require('./routes/auth')
+const userRoutes=require('./routes/user')
+const categoryRoutes=require('./routes/category')
+const productRoutes=require('./routes/product')
+mongoose.connect('mongodb://localhost/playground')
+.then(()=>console.log("connected to mongodb..."))
+.catch(()=>console.error("could not connect to mongodb"))
+
+
+//set config
+require('dotenv').config()
+//middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+//routes middleware
+app.use('/api',authRoutes);
+app.use('/api',userRoutes);
+app.use('/api',categoryRoutes);
+app.use('/api',productRoutes);
+
+const port =process.env.PORT||8000;
+app.listen(port,()=>{
+    console.log(`server start at port ${port}`);
+})
---
+++ routes/product.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const req = require('express/lib/request');
+const router=express.Router()
+const{ create,productById,read, Remove,Update,List,listRelated,listCategories,listBySearch,photo }=require('../controllers/product');
+const{requireSignin,isAuth,isAdmin }=require('../controllers/auth');
+const{ userById }=require('../controllers/user');
+const { remove } = require('lodash');
+router.get("/product/:productId",read)
+router.post("/product/create/:userId",requireSignin,isAuth,isAdmin,create);
+router.delete('/product/:productId/:userId',requireSignin,isAuth,isAdmin,Remove)
+router.put('/product/:productId/:userId',requireSignin,isAuth,isAdmin,Update)
+router.get('/products',List)
+router.get('/products/related/:productId',listRelated)
+router.get('/products/categories',listCategories)
+router.post("/products/by/search", listBySearch);
+router.get('/product/photo/:productId',photo)
+router.param('userId',userById)
+router.param('productId',productById)
+
+
+module.exports=router
